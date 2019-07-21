07b7b1fe940e59db0e1791e083782a9d6b2a25f8
nongsansanee <nong_com@hotmail.com>
2019-07-21 17:12:09 +0700

    create database and create relation divisions ,patients,treatments

--- app/Patient.php
+++ app/Patient.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Patient extends Model
 {
-    //
+    public function division(){
+        return $this->belongsTo(Division::class);
+    }
+
+    public function treatments(){
+        return $this->hasMany(Treatment::class);
+    }
 }
--- app/Treatment.php
+++ app/Treatment.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Treatment extends Model
 {
-    //
+    public function patient(){
+        return $this->belongsTo(Patient::class);
+    }
 }
